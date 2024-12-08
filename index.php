980ab041e22ba7638e7e79a5137f8ff394efd511
Notnochka <pali4an@gmail.com>
2024-12-08 15:29:33 +0700

    форматирование суммы

--- index.php
+++ index.php
@@ -41,6 +41,16 @@ $items =[
         'pic' => 'img/lot-6.jpg'
     ]
 ];
+
+function format(float $amount): string {
+    $amount = ceil($amount);
+    if ($amount >= 1000) {
+        $formattedAmount = number_format($amount, 0, '', ' ');
+    } else {
+        $formattedAmount = (string)$amount;
+    }
+    return $formattedAmount . ' ₽';
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -118,7 +128,7 @@ $items =[
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price'];?><b class="rub">₽</b></span>
+                            <span class="lot__cost"><?php echo format($value['price']);?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
