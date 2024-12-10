a7cc27a68c0a5514d237fed767ff2d3899f9844c
Notnochka <pali4an@gmail.com>
2024-12-11 05:14:19 +0700

    подправил цикл foreach

--- templates/main.php
+++ templates/main.php
@@ -18,18 +18,18 @@
         <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-        <?php foreach ($lots as $lot => $value): ?>
+        <?php foreach ($lots as $lot): ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=$value['pic'];?>" width="350" height="260" alt="">
+                <img src="<?=$lot['pic'];?>" width="350" height="260" alt="">
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?= htmlspecialchars($value['category']);?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name'];?></a></h3>
+                <span class="lot__category"><?= htmlspecialchars($lot['category']);?></span>
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['name'];?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?php echo format(htmlspecialchars($value['price']));?></span>
+                        <span class="lot__cost"><?php echo format(htmlspecialchars($lot['price']));?></span>
                     </div>
                     <div class="lot__timer timer">
                         12:23
